b57aa0fc2b5e7224064dd69e4d692e97b31199b0
Bisaso Ronald <bronnie313@gmail.com>
2023-08-08 13:40:26 +0300

    define the musicAlbum methods

--- musicAlbum/genre.rb
+++ musicAlbum/genre.rb
@@ -1,4 +1,9 @@
 class Genre
+    attr_reader: name
+
+    private 
+    attr_reader :id, :items
+
     def initialize(id, name )
         @id = id
         @name = name 
--- musicAlbum/musicAlbum.rb
+++ musicAlbum/musicAlbum.rb
@@ -1,8 +1,12 @@
 class MusicAlbum 
+    attr_reader :on_spotify
+
     def initialize( on_spotify: true)
-        @on_spotify = true
+        @on_spotify = on_spotify
     end
+
+    private
     
-    can_be_archived?()
+    def can_be_archived?()
     end
 end
