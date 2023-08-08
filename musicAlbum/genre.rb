9bc370085dcb39ac5543a157484206327ff62f12
Bisaso Ronald <bronnie313@gmail.com>
2023-08-08 13:13:57 +0300

    define genre methods

--- musicAlbum/genre.rb
+++ musicAlbum/genre.rb
@@ -1,2 +1,11 @@
 class Genre
+    def initialize(id, name )
+        @id = id
+        @name = name 
+        @items = []
+    end
+
+    def add_item(item)
+        @items << item
+    end
 end
--- musicAlbum/musicAlbum.rb
+++ musicAlbum/musicAlbum.rb
@@ -1,2 +1,8 @@
 class MusicAlbum 
+    def initialize( on_spotify: true)
+        @on_spotify = true
+    end
+    
+    can_be_archived?()
+    end
 end
