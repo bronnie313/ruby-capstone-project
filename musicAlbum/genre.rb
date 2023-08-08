da28f2b1c73a03c9cc7ab0a2dc3f999bcb7cd309
Bisaso Ronald <bronnie313@gmail.com>
2023-08-08 18:23:34 +0300

    add initial tests

--- musicAlbum/genre.rb
+++ musicAlbum/genre.rb
@@ -1,5 +1,5 @@
 class Genre
-  attr_reader :name :item
+  attr_reader :name, :item
 
   private
 
---
+++ musicAlbum/genre_spec.rb
@@ -0,0 +1,16 @@
+require_relative 'genre'  # Adjust the path to the Genre class
+
+describe Genre do
+  let(:genre) { Genre.new(1, 'Rock') }
+
+  context "when initializing a genre" do
+    it "should have the correct id and name" do
+      expect(genre.instance_variable_get(:@id)).to eq(1)
+      expect(genre.instance_variable_get(:@name)).to eq('Rock')
+    end
+
+    it "should have an empty items array initially" do
+      expect(genre.instance_variable_get(:@items)).to be_empty
+    end
+  end
+end
